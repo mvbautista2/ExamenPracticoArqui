90a091674cd924ae8320d2a889fbb490bb6e0198
Valentina Bautista <valentina_bautista@hotmail.com>
2022-01-19 12:53:35 -0500

    Examen 2p Arqui

---
+++ BautistaExamen2PArqui/src/main/java/ec/edu/espe/arqsoftware/Examen3P/Examen3PApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqsoftware.Examen3P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen3PApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen3PApplication.class, args);
+	}
+
+}
---
+++ BautistaExamen2PArqui/src/test/java/ec/edu/espe/arqsoftware/Examen3P/Examen3PApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqsoftware.Examen3P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen3PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
